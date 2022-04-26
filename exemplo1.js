5bc391792f07a95a60429d5fc48464e68ff05ff0
N3ROHz <83547051+N3ROHz@users.noreply.github.com>
2022-04-26 09:14:23 -0300

    Add files via upload

---
+++ exemplo1.js
@@ -0,0 +1,45 @@
+var scene;
+var camera;
+var renderer;
+
+var cube;
+
+
+var init = function() {
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    this.createACube();
+
+    camera.position.z = 5;
+
+    this.render();
+
+};
+
+var render = function() {
+    requestAnimationFrame( render );
+
+    this.animateCube();
+
+    renderer.render( scene, camera );
+};
+
+var createACube = function() {
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: "red" } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+};
+
+var animateCube = function() {
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+};
+
+window.onload = this.init;
